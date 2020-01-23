d2cd4dd28f04efc2d6c70f7197289223a6f800cb
LawrE18 <egor_lawr@tut.by>
2020-01-23 13:34:08 +0300

    Version 3.0

--- test/belt.py
+++ test/belt.py
@@ -24,11 +24,11 @@ def beltBlockEncr(block, key):
 def beltBlockDecr(block, key):
 	assert len(block) * 8 == 128
 
-	plain = block.encode('base64').encode()
-	key = key.encode('hex').encode()
+	plain = b64_encoder(block)[0].encode()
+	key = hex_encoder(key)[0].encode()
 	key_bitlen = len(key) * 4
 
-	prefix = ECHO_EXE_PATH + ' ' + plain[:-1] + ' | ' + OPENSSL_EXE_PATH + ' enc -d -base64 |'
+	prefix = ECHO_EXE_PATH + ' ' + plain[:-1] + ' | python -m base64 -d |'
 	cmd = 'enc -d -belt-ecb{} -nosalt -nopad -K {}'.format(key_bitlen, key)
 	retcode, block, er__ = openssl(cmd, prefix, True)
 	return block
@@ -36,11 +36,11 @@ def beltBlockDecr(block, key):
 def beltECBEncr(src, key):
 	assert (len(src) * 8) % 128 == 0
 
-	plain = src.encode('base64')
-	key = key.encode('hex')
+	plain = b64_encoder(src)[0].encode()
+	key = hex_encoder(key)[0].encode()
 	key_bitlen = len(key) * 4
 
-	prefix = ECHO_EXE_PATH + ' ' + plain[:-1] + ' | ' + OPENSSL_EXE_PATH + ' enc -d -base64 |'
+	prefix = ECHO_EXE_PATH + ' ' + plain[:-1] + ' | python -m base64 -d |'
 	cmd = 'enc -e -belt-ecb{} -nosalt -nopad -K {}'.format(key_bitlen, key)
 	retcode, dest, er__ = openssl(cmd, prefix, True)
 	return dest
@@ -48,11 +48,11 @@ def beltECBEncr(src, key):
 def beltECBDecr(src, key):
 	assert (len(src) * 8) % 128 == 0
 
-	plain = src.encode('base64')
-	key = key.encode('hex')
+	plain = b64_encoder(src)[0].encode()
+	key = hex_encoder(key)[0].encode()
 	key_bitlen = len(key) * 4
 
-	prefix = ECHO_EXE_PATH + ' ' + plain[:-1] + ' | ' + OPENSSL_EXE_PATH + ' enc -d -base64 |'
+	prefix = ECHO_EXE_PATH + ' ' + plain[:-1] + ' | python -m base64 -d |'
 	cmd = 'enc -d -belt-ecb{} -nosalt -nopad -K {}'.format(key_bitlen, key)
 	retcode, dest, er__ = openssl(cmd, prefix, True)
 	return dest	
@@ -60,12 +60,12 @@ def beltECBDecr(src, key):
 def beltCBCEncr(src, key, iv):
 	assert (len(src) * 8) % 128 == 0
 
-	plain = src.encode('base64')
-	key = key.encode('hex')
-	iv = iv.encode('hex')
+	plain = b64_encoder(src)[0].encode()
+	key = hex_encoder(key)[0].encode()
+	iv = hex_encoder(iv)[0].encode()
 	key_bitlen = len(key) * 4
 
-	prefix = ECHO_EXE_PATH + ' ' + plain[:-1] + ' | ' + OPENSSL_EXE_PATH + ' enc -d -base64 |'
+	prefix = ECHO_EXE_PATH + ' ' + plain[:-1] + ' | python -m base64 -d |'
 	cmd = 'enc -e -belt-cbc{} -nosalt -nopad -K {} -iv {}'.format(key_bitlen, key, iv)
 	retcode, dest, er__ = openssl(cmd, prefix, True)
 	return dest
@@ -73,12 +73,12 @@ def beltCBCEncr(src, key, iv):
 def beltCBCDecr(src, key, iv):
 	assert (len(src) * 8) % 128 == 0
 
-	plain = src.encode('base64')
-	key = key.encode('hex')
-	iv = iv.encode('hex')
+	plain = b64_encoder(src)[0].encode()
+	key = hex_encoder(key)[0].encode()
+	iv = hex_encoder(iv)[0].encode()
 	key_bitlen = len(key) * 4
 
-	prefix = ECHO_EXE_PATH + ' ' + plain[:-1] + ' | ' + OPENSSL_EXE_PATH + ' enc -d -base64 |'
+	prefix = ECHO_EXE_PATH + ' ' + plain[:-1] + ' | python -m base64 -d |'
 	cmd = 'enc -d -belt-cbc{} -nosalt -nopad -K {} -iv {}'.format(key_bitlen, key, iv)
 	retcode, dest, er__ = openssl(cmd, prefix, True)
 	return dest
@@ -86,12 +86,12 @@ def beltCBCDecr(src, key, iv):
 def beltCFBEncr(src, key, iv):
 	assert (len(src) * 8) % 128 == 0
 
-	plain = src.encode('base64')
-	key = key.encode('hex')
-	iv = iv.encode('hex')
+	plain = b64_encoder(src)[0].encode()
+	key = hex_encoder(key)[0].encode()
+	iv = hex_encoder(iv)[0].encode()
 	key_bitlen = len(key) * 4
 
-	prefix = ECHO_EXE_PATH + ' ' + plain[:-1] + ' | ' + OPENSSL_EXE_PATH + ' enc -d -base64 |'
+	prefix = ECHO_EXE_PATH + ' ' + plain[:-1] + ' | python -m base64 -d |'
 	cmd = 'enc -e -belt-cfb{} -nosalt -nopad -K {} -iv {}'.format(key_bitlen, key, iv)
 	retcode, dest, er__ = openssl(cmd, prefix, True)
 	return dest
@@ -99,12 +99,12 @@ def beltCFBEncr(src, key, iv):
 def beltCFBDecr(src, key, iv):
 	assert (len(src) * 8) % 128 == 0
 
-	plain = src.encode('base64')
-	key = key.encode('hex')
-	iv = iv.encode('hex')
+	plain = b64_encoder(src)[0].encode()
+	key = hex_encoder(key)[0].encode()
+	iv = hex_encoder(iv)[0].encode()
 	key_bitlen = len(key) * 4
 
-	prefix = ECHO_EXE_PATH + ' ' + plain[:-1] + ' | ' + OPENSSL_EXE_PATH + ' enc -d -base64 |'
+	prefix = ECHO_EXE_PATH + ' ' + plain[:-1] + ' | python -m base64 -d |'
 	cmd = 'enc -d -belt-cfb{} -nosalt -nopad -K {} -iv {}'.format(key_bitlen, key, iv)
 	retcode, dest, er__ = openssl(cmd, prefix, True)
 	return dest
@@ -112,12 +112,12 @@ def beltCFBDecr(src, key, iv):
 def beltCTREncr(src, key, iv):
 	assert (len(src) * 8) % 128 == 0
 
-	plain = src.encode('base64')
-	key = key.encode('hex')
-	iv = iv.encode('hex')
+	plain = b64_encoder(src)[0].encode()
+	key = hex_encoder(key)[0].encode()
+	iv = hex_encoder(iv)[0].encode()
 	key_bitlen = len(key) * 4
 
-	prefix = ECHO_EXE_PATH + ' ' + plain[:-1] + ' | ' + OPENSSL_EXE_PATH + ' enc -d -base64 |'
+	prefix = ECHO_EXE_PATH + ' ' + plain[:-1] + ' | python -m base64 -d |'
 	cmd = 'enc -e -belt-ctr{} -nosalt -nopad -K {} -iv {}'.format(key_bitlen, key, iv)
 	retcode, dest, er__ = openssl(cmd, prefix, True)
 	return dest
@@ -125,35 +125,35 @@ def beltCTREncr(src, key, iv):
 def beltCTRDecr(right_plain_msg, right_enc_msg, key, msg_len, iv):
 	assert (len(src) * 8) % 128 == 0
 
-	plain = src.encode('base64')
-	key = key.encode('hex')
-	iv = iv.encode('hex')
+	plain = b64_encoder(src)[0].encode()
+	key = hex_encoder(key)[0].encode()
+	iv = hex_encoder(iv)[0].encode()
 	key_bitlen = len(key) * 4
 
-	prefix = ECHO_EXE_PATH + ' ' + plain[:-1] + ' | ' + OPENSSL_EXE_PATH + ' enc -d -base64 |'
+	prefix = ECHO_EXE_PATH + ' ' + plain[:-1] + ' | python -m base64 -d |'
 	cmd = 'enc -d -belt-ctr{} -nosalt -nopad -K {} -iv {}'.format(key_bitlen, key, iv)
 	retcode, dest, er__ = openssl(cmd, prefix, True)
 	return dest
 
 def beltMAC(src, key):
-	plain = src.encode('base64')
-	key = key.encode('hex')
+	plain = b64_encoder(src)[0].encode()
+	key = hex_encoder(key)[0].encode()
 	key_bitlen = len(key)*4
 	
-	prefix = ECHO_EXE_PATH + ' ' + plain[:-1] + ' | ' + OPENSSL_EXE_PATH + ' enc -d -base64 |'
+	prefix = ECHO_EXE_PATH + ' ' + plain[:-1] + ' | python -m base64 -d |'
 	cmd = 'dgst -mac belt-mac{} -macopt hexkey:{}'.format(256, key)
 	retcode, out, er__ = openssl(cmd, prefix, True)
 	mac = out.split(' ')[1][:-1]
-	return bytes(mac.decode('hex'))
+	return bytes(hex_decoder(mac)[0])
 	
 def beltHash(src):
-	plain = src.encode('base64')
+	plain = b64_encoder(src)[0].encode()
 
-	prefix = ECHO_EXE_PATH + ' ' + plain[:-1] + ' | ' + OPENSSL_EXE_PATH + ' enc -d -base64 |'
+	prefix = ECHO_EXE_PATH + ' ' + plain[:-1] + ' | python -m base64 -d |'
 	cmd = 'dgst -belt-hash'.format()
 	retcode, out, er__ = openssl(cmd, prefix, True)
 	hash_ = out.split(' ')[1][:-1]
-	return bytes(hash_.decode('hex'))
+	return bytes(hex_decoder(hash_)[0])
 
 
 
--- test/test.py
+++ test/test.py
@@ -31,76 +31,76 @@ def test_belt():
 	block = beltBlockEncr(bytes(block), bytes(key))
 	assert hex_encoder(block)[0].decode() == '69cca1c93557c9e3d66bc3e0fa88fa6e'
 
-	'''#A.4 Decrypt
-	block = 'e12bdc1ae28257ec703fccf095ee8df1'.decode('hex')
-	key = ('92bd9b1ce5d141015445fbc95e4d0ef2' + '682080aa227d642f2687f93490405511').decode('hex')
+	#A.4 Decrypt
+	block = hex_decoder('e12bdc1ae28257ec703fccf095ee8df1')[0]
+	key = hex_decoder('92bd9b1ce5d141015445fbc95e4d0ef2' + '682080aa227d642f2687f93490405511')[0]
 	block = beltBlockDecr(bytes(block), bytes(key))
-	assert block.encode('hex') == '0dc5300600cab840b38448e5e993f421'
+	assert hex_encoder(block)[0].decode() == '0dc5300600cab840b38448e5e993f421'
 	
 	#ECB (|X| = 384)
 	#A.6 Encrypt
-	src = ('b194bac80a08f53b366d008e584a5de4' + '8504fa9d1bb6c7ac252e72c202fdce0d' + '5be3d61217b96181fe6786ad716b890b').decode('hex')
-	key = ('e9dee72c8f0c0fa62ddb49f46f739647' + '06075316ed247a3739cba38303a98bf6').decode('hex')
+	src = hex_decoder('b194bac80a08f53b366d008e584a5de4' + '8504fa9d1bb6c7ac252e72c202fdce0d' + '5be3d61217b96181fe6786ad716b890b')[0]
+	key = hex_decoder('e9dee72c8f0c0fa62ddb49f46f739647' + '06075316ed247a3739cba38303a98bf6')[0]
 	dest = beltECBEncr(bytes(src), bytes(key))
-	assert dest.encode('hex') == ('69cca1c93557c9e3d66bc3e0fa88fa6e' + '5f23102ef109710775017f73806da9dc' + '46fb2ed2ce771f26dcb5e5d1569f9ab0')
+	assert hex_encoder(dest)[0].decode() == ('69cca1c93557c9e3d66bc3e0fa88fa6e' + '5f23102ef109710775017f73806da9dc' + '46fb2ed2ce771f26dcb5e5d1569f9ab0')
 
 	#A.8 Decrypt	
-	src = ('e12bdc1ae28257ec703fccf095ee8df1' + 'c1ab76389fe678caf7c6f860d5bb9c4f' + 'f33c657b637c306add4ea7799eb23d31').decode('hex')
-	key = ('92bd9b1ce5d141015445fbc95e4d0ef2' + '682080aa227d642f2687f93490405511').decode('hex')
+	src = hex_decoder('e12bdc1ae28257ec703fccf095ee8df1' + 'c1ab76389fe678caf7c6f860d5bb9c4f' + 'f33c657b637c306add4ea7799eb23d31')[0]
+	key = hex_decoder('92bd9b1ce5d141015445fbc95e4d0ef2' + '682080aa227d642f2687f93490405511')[0]
 	dest = beltECBDecr(bytes(src), bytes(key))
-	assert dest.encode('hex') == ('0dc5300600cab840b38448e5e993f421' + 'e55a239f2ab5c5d5fdb6e81b40938e2a' + '54120ca3e6e19c7ad750fc3531daeab7')
+	assert hex_encoder(dest)[0].decode() == ('0dc5300600cab840b38448e5e993f421' + 'e55a239f2ab5c5d5fdb6e81b40938e2a' + '54120ca3e6e19c7ad750fc3531daeab7')
 
 	#CBC (|X| = 384)
 	#A.10 Encrypt
-	src = ('b194bac80a08f53b366d008e584a5de4' + '8504fa9d1bb6c7ac252e72c202fdce0d' + '5be3d61217b96181fe6786ad716b890b').decode('hex')
-	iv = 'be32971343fc9a48a02a885f194b09a1'.decode('hex')
-	key = ('e9dee72c8f0c0fa62ddb49f46f739647' + '06075316ed247a3739cba38303a98bf6').decode('hex')
+	src = hex_decoder('b194bac80a08f53b366d008e584a5de4' + '8504fa9d1bb6c7ac252e72c202fdce0d' + '5be3d61217b96181fe6786ad716b890b')[0]
+	iv = hex_decoder('be32971343fc9a48a02a885f194b09a1')[0]
+	key = hex_decoder('e9dee72c8f0c0fa62ddb49f46f739647' + '06075316ed247a3739cba38303a98bf6')[0]
 	dest = beltCBCEncr(bytes(src), bytes(key), bytes(iv))
-	assert dest.encode('hex') == ('10116efae6ad58ee14852e11da1b8a74' + '5cf2480e8d03f1c19492e53ed3a70f60' + '657c1ee8c0e0ae5b58388bf8a68e3309')
+	assert hex_encoder(dest)[0].decode() == ('10116efae6ad58ee14852e11da1b8a74' + '5cf2480e8d03f1c19492e53ed3a70f60' + '657c1ee8c0e0ae5b58388bf8a68e3309')
 
 	#A.12 Decrypt
-	src = ('e12bdc1ae28257ec703fccf095ee8df1' + 'c1ab76389fe678caf7c6f860d5bb9c4f' + 'f33c657b637c306add4ea7799eb23d31').decode('hex')
-	iv = '7ecda4d01544af8ca58450bf66d2e88a'.decode('hex')
-	key = ('92bd9b1ce5d141015445fbc95e4d0ef2' + '682080aa227d642f2687f93490405511').decode('hex')
+	src = hex_decoder('e12bdc1ae28257ec703fccf095ee8df1' + 'c1ab76389fe678caf7c6f860d5bb9c4f' + 'f33c657b637c306add4ea7799eb23d31')[0]
+	iv = hex_decoder('7ecda4d01544af8ca58450bf66d2e88a')[0]
+	key = hex_decoder('92bd9b1ce5d141015445fbc95e4d0ef2' + '682080aa227d642f2687f93490405511')[0]
 	dest = beltCBCDecr(bytes(src), bytes(key), bytes(iv))
-	assert dest.encode('hex') == ('730894d6158e17cc1600185a8f411cab' + '0471ff85c83792398d8924ebd57d03db' + '95b97a9b7907e4b020960455e46176f8')
+	assert hex_encoder(dest)[0].decode() == ('730894d6158e17cc1600185a8f411cab' + '0471ff85c83792398d8924ebd57d03db' + '95b97a9b7907e4b020960455e46176f8')
 
 	#CFB (|X| = 384)
 	#A.14 Encrypt
-	src = ('b194bac80a08f53b366d008e584a5de4' + '8504fa9d1bb6c7ac252e72c202fdce0d' + '5be3d61217b96181fe6786ad716b890b').decode('hex')
-	iv = 'be32971343fc9a48a02a885f194b09a1'.decode('hex')
-	key = ('e9dee72c8f0c0fa62ddb49f46f739647' + '06075316ed247a3739cba38303a98bf6').decode('hex')
+	src = hex_decoder('b194bac80a08f53b366d008e584a5de4' + '8504fa9d1bb6c7ac252e72c202fdce0d' + '5be3d61217b96181fe6786ad716b890b')[0]
+	iv = hex_decoder('be32971343fc9a48a02a885f194b09a1')[0]
+	key = hex_decoder('e9dee72c8f0c0fa62ddb49f46f739647' + '06075316ed247a3739cba38303a98bf6')[0]
 	dest = beltCFBEncr(bytes(src), bytes(key), bytes(iv))
-	assert dest.encode('hex') == ('c31e490a90efa374626cc99e4b7b8540' + 'a6e48685464a5a06849c9ca769a1b0ae' + '55c2cc5939303ec832dd2fe16c8e5a1b')
+	assert hex_encoder(dest)[0].decode() == ('c31e490a90efa374626cc99e4b7b8540' + 'a6e48685464a5a06849c9ca769a1b0ae' + '55c2cc5939303ec832dd2fe16c8e5a1b')
 
 	#A.15 Decrypt
-	src = ('e12bdc1ae28257ec703fccf095ee8df1' + 'c1ab76389fe678caf7c6f860d5bb9c4f' + 'f33c657b637c306add4ea7799eb23d31').decode('hex') 
-	iv = '7ecda4d01544af8ca58450bf66d2e88a'.decode('hex')
-	key = ('92bd9b1ce5d141015445fbc95e4d0ef2' + '682080aa227d642f2687f93490405511').decode('hex')
+	src = hex_decoder('e12bdc1ae28257ec703fccf095ee8df1' + 'c1ab76389fe678caf7c6f860d5bb9c4f' + 'f33c657b637c306add4ea7799eb23d31')[0]
+	iv = hex_decoder('7ecda4d01544af8ca58450bf66d2e88a')[0]
+	key = hex_decoder('92bd9b1ce5d141015445fbc95e4d0ef2' + '682080aa227d642f2687f93490405511')[0]
 	dest = beltCFBDecr(bytes(src), bytes(key), bytes(iv))
-	assert dest.encode('hex') == ('fa9d107a86f375ee65cd1db881224bd0' + '16aff814938ed39b3361abb0bf0851b6' + '52244eb06842dd4c94aa4500774e40bb')
+	assert hex_encoder(dest)[0].decode() == ('fa9d107a86f375ee65cd1db881224bd0' + '16aff814938ed39b3361abb0bf0851b6' + '52244eb06842dd4c94aa4500774e40bb')
 
 	#CTR (|X| = 384)
 	#A.16
-	src = ('b194bac80a08f53b366d008e584a5de4' + '8504fa9d1bb6c7ac252e72c202fdce0d' + '5be3d61217b96181fe6786ad716b890b').decode('hex')
-	iv = 'be32971343fc9a48a02a885f194b09a1'.decode('hex')
-	key = ('e9dee72c8f0c0fa62ddb49f46f739647' + '06075316ed247a3739cba38303a98bf6').decode('hex')
+	src = hex_decoder('b194bac80a08f53b366d008e584a5de4' + '8504fa9d1bb6c7ac252e72c202fdce0d' + '5be3d61217b96181fe6786ad716b890b')[0]
+	iv = hex_decoder('be32971343fc9a48a02a885f194b09a1')[0]
+	key = hex_decoder('e9dee72c8f0c0fa62ddb49f46f739647' + '06075316ed247a3739cba38303a98bf6')[0]
 	dest = beltCTREncr(bytes(src), bytes(key), bytes(iv))
-	assert dest.encode('hex') == ('52c9af96ff50f64435fc43def56bd797' + 'd5b5b1ff79fb41257ab9cdf6e63e81f8' + 'f00341473eae409833622de05213773a')
+	assert hex_encoder(dest)[0].decode() == ('52c9af96ff50f64435fc43def56bd797' + 'd5b5b1ff79fb41257ab9cdf6e63e81f8' + 'f00341473eae409833622de05213773a')
 
 	#MAC
 	#A.18
-	src = ('b194bac80a08f53b366d008e584a5de4' + '8504fa9d1bb6c7ac252e72c202fdce0d' + '5be3d61217b96181fe6786ad716b890b').decode('hex')
-	key = ('e9dee72c8f0c0fa62ddb49f46f739647' + '06075316ed247a3739cba38303a98bf6').decode('hex')
+	src = hex_decoder('b194bac80a08f53b366d008e584a5de4' + '8504fa9d1bb6c7ac252e72c202fdce0d' + '5be3d61217b96181fe6786ad716b890b')[0]
+	key = hex_decoder('e9dee72c8f0c0fa62ddb49f46f739647' + '06075316ed247a3739cba38303a98bf6')[0]
 	mac = beltMAC(bytes(src), bytes(key))
-	assert mac.encode('hex') == '2dab59771b4b16d0'
+	assert hex_encoder(mac)[0].decode()  == '2dab59771b4b16d0'
 
 	#HASH
 	#A.25
-	src = ('b194bac80a08f53b366d008e584a5de4' + '8504fa9d1bb6c7ac252e72c202fdce0d').decode('hex')
+	src = hex_decoder('b194bac80a08f53b366d008e584a5de4' + '8504fa9d1bb6c7ac252e72c202fdce0d')[0]
 	hash_ = beltHash(bytes(src))
-	assert hash_.encode('hex') == ('749e4c3653aece5e48db4761227742eb' + '6dbe13f4a80f7beff1a9cf8d10ee7786')
-	'''
+	assert hex_encoder(hash_)[0].decode()  == ('749e4c3653aece5e48db4761227742eb' + '6dbe13f4a80f7beff1a9cf8d10ee7786')
+
 
 if __name__ == '__main__':
 	test_version()
