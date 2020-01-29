f85204152f4281cae0b89ef9bccd0741eb4d6b65
LawrE18 <egor_lawr@tut.by>
2020-01-29 13:34:09 +0300

    Add logs

--- test/settings.py
+++ test/settings.py
@@ -1,3 +1,14 @@
+# *****************************************************************************
+# \file settings.py
+# \project bee2evp [EVP-interfaces over bee2 / engine of OpenSSL]
+# \brief A settings needed for testing
+# \created 2020.01.27
+# \version 2020.01.29
+# \license This program is released under the GNU General Public License 
+# version 3 with the additional exemption that compiling, linking, 
+# and/or using OpenSSL is allowed. See Copyright Notices in bee2evp/info.h.
+# *****************************************************************************
+
 import codecs
 
 hex_encoder = codecs.getencoder('hex')
--- test/test.py
+++ test/test.py
@@ -3,7 +3,7 @@
 # \project bee2evp [EVP-interfaces over bee2 / engine of OpenSSL]
 # \brief Python tests for openssl[bee2evp]
 # \created 2019.07.10
-# \version 2019.07.16
+# \version 2020.01.29
 # \license This program is released under the GNU General Public License 
 # version 3 with the additional exemption that compiling, linking, 
 # and/or using OpenSSL is allowed. See Copyright Notices in bee2evp/info.h.
