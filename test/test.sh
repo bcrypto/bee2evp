4e4e70e14e7a9a457bf7859a25ea7e08bb4725a3
Sergey Agievich <agievich@gmail.com>
2024-05-30 19:20:30 +0300

    Update test/test.sh

--- test/test.sh
+++ test/test.sh
@@ -4,7 +4,7 @@
 # \project bee2evp [EVP-interfaces over bee2 / engine of OpenSSL]
 # \brief Scripts for building and testing
 # \created 2020.07.10
-# \version 2023.10.02
+# \version 2024.05.29
 # \copyright The Bee2evp authors
 # \license Licensed under the Apache License, Version 2.0 (see LICENSE.txt).
 # *****************************************************************************
@@ -14,9 +14,9 @@ bee2evp=$(pwd)/..
 build_root=$bee2evp/build
 bee2=$bee2evp/bee2
 openssl=$bee2evp/openssl
-build_bee2evp=$build_root/build_bee2evp
-build_bee2=$build_root/build_bee2
-build_openssl=$build_root/build_openssl
+build_bee2evp=$build_root
+build_bee2=$build_root/bee2
+build_openssl=$build_root/openssl
 local=$build_root/local
 
 openssl_branch=OpenSSL_1_1_1i
@@ -34,7 +34,8 @@ clean(){
 update_repos(){
   echo $openssl_branch
   git submodule update --init
-  git clone -b $openssl_branch --depth 1 https://github.com/openssl/openssl $openssl
+  git clone -b $openssl_branch --depth 1  https://github.com/openssl/openssl\
+    $openssl
 }
 
 patch_openssl(){
@@ -45,14 +46,16 @@ patch_openssl(){
 }
 
 build_bee2(){
-  mkdir -p $build_bee2 && mkdir -p $local && cd $build_bee2
-  cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_PIC=ON -DCMAKE_INSTALL_PREFIX=$local $bee2
+  mkdir -p $build_bee2 && cd $build_bee2
+  cmake -DCMAKE_BUILD_TYPE=Release \
+    -DBUILD_PIC=ON \
+    -DCMAKE_INSTALL_PREFIX=$local $bee2
   make -j$(nproc) && make install
   ls -la $local/lib/libbee2_static.a
 }
 
 build_openssl(){
-  mkdir -p $build_openssl && mkdir -p $local && cd $build_openssl
+  mkdir -p $build_openssl && cd $build_openssl
   $openssl/config shared -d --prefix=$local --openssldir=$local
   make -j$(nproc) all
   make install > build.log 2>&1 || (cat build.log && exit 1)
@@ -66,31 +69,45 @@ build_openssl(){
 build_bee2evp(){
   mkdir -p $build_bee2evp && cd $build_bee2evp
   cmake -DCMAKE_BUILD_TYPE=Release \
-    -DBEE2_LIBRARY_DIRS=$local/lib -DBEE2_INCLUDE_DIRS=$local/include \
-    -DOPENSSL_LIBRARY_DIRS=$local/lib -DOPENSSL_INCLUDE_DIRS=$local/include \
-    -DLIB_INSTALL_DIR=$local/lib -DCMAKE_INSTALL_PREFIX=$local $bee2evp
+    -DBEE2_LIBRARY_DIRS=$local/lib \
+    -DBEE2_INCLUDE_DIRS=$local/include \
+    -DOPENSSL_LIBRARY_DIRS=$local/lib \
+    -DOPENSSL_INCLUDE_DIRS=$local/include \
+    -DLIB_INSTALL_DIR=$local/lib \
+    -DCMAKE_INSTALL_PREFIX=$local $bee2evp
   make -j$(nproc) && make install
   ls -la $local/lib/libbee2evp.so
 }
 
 attach_bee2evp(){
-  mv $local/openssl.cnf.dist $local/openssl.cnf
-  sed -i "/\[ new\_oids\ ]/i openssl_conf = openssl_init\n[ openssl_init ]\nengines = engine_section\n[ engine_section ]\nbee2evp = bee2evp_section\n[ bee2evp_section ]\nengine_id = bee2evp\ndynamic_path = $local/lib/libbee2evp.so\ndefault_algorithms = ALL" $local/openssl.cnf
+  cp $local/openssl.cnf.dist $local/openssl.cnf
+  sed -i "/\[ new\_oids \]/i openssl_conf = openssl_init\
+    \n[ openssl_init ]\
+    \nengines = engine_section\
+    \n[ engine_section ]\
+    \nbee2evp = bee2evp_section\
+    \n[ bee2evp_section ]\
+    \nengine_id = bee2evp\
+    \ndynamic_path = $local/lib/libbee2evp.so\
+    \ndefault_algorithms = ALL\
+    \n" $local/openssl.cnf
 }
 
 test_bee2evp(){
-  export LD_LIBRARY_PATH="$local/lib:${LD_LIBRARY_PATH:-}"
   cd $local || exit
   cp -a ../../test/. .
+  export LD_LIBRARY_PATH="$local/lib:${LD_LIBRARY_PATH:-}"
   python test.py
+  export LD_LIBRARY_PATH=$(echo "$LD_LIBRARY_PATH" | \
+    sed -e "s|$local/lib:||")
 }
 
-#install_prereq
-#clean
-#update_repos
-#patch_openssl
-#build_bee2
-#build_openssl
-#build_bee2evp
-#attach_bee2evp
+install_prereq
+clean
+update_repos
+patch_openssl
+build_bee2
+build_openssl
+build_bee2evp
+attach_bee2evp
 test_bee2evp
