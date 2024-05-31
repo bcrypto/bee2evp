f7aeb68a9f333622dbb031d2f5c1a2f5079a8ec4
Sergey Agievich <agievich@gmail.com>
2024-05-31 13:13:25 +0300

    Split test/test.sh into two parts

--- test/build.sh
+++ test/build.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 # *****************************************************************************
-# \file test.sh
+# \file build.sh
 # \project bee2evp [EVP-interfaces over bee2 / engine of OpenSSL]
 # \brief Build script
 # \created 2024.05.31
 # \version 2024.05.31
 # \copyright The Bee2evp authors
 # \license Licensed under the Apache License, Version 2.0 (see LICENSE.txt).
-# \pre build.sh
 # *****************************************************************************
 
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 source source.sh
 
-test_bee2evp
+install_prereq && clean && update_repos && patch_openssl &&\
+  build_bee2 && build_openssl && build_bee2evp && attach_bee2evp
