12cf602955deb47ce237c17157bd7121701d0caa
LawrE18 <Murakami18@yandex.ru>
2024-09-05 23:07:15 +0300

    Remove commented debug code

--- btls/patch/patching.py
+++ btls/patch/patching.py
@@ -17,8 +17,6 @@ import multiline
 
 def search_array(content, array_name):
     start_idx = re.search(rf"static.*{array_name}", content).start()
-    # content.find(fr'static.*{array_name}')
-    # start_idx = content.find('{', start_idx)
     end_idx = content.find('};', start_idx)
     return content[start_idx:end_idx + 1]
 
@@ -31,13 +29,8 @@ def search_macro(content, macro_name):
 
 def search_func(content, func_name):
     start_idx = re.search(rf"\w+\s\**{func_name}[^\;^\n]*\n", content).start()
-    # end_idx = re.search(r"return\s+\w+;\n+\}", content[start_idx:]).end()
     end_idx = re.search(r"\n\}\n", content[start_idx:]).end()
-    #if func_name == "tls_process_client_key_exchange":
-    #    print(content[start_idx:end_idx + start_idx + 1])
 
-    # print('############################')
-    # func_code = re.search(rf"\w+\s{func_name}", content)
     return content[start_idx:end_idx + start_idx + 1]
 
 
@@ -53,7 +46,6 @@ class Patch:
             patch = f.read()
 
         res = multiline.loads(patch, multiline=True)
-        # print(str(res))
 
         files = res.keys()
         for file in files:
@@ -84,7 +76,6 @@ class Patch:
                 content = content[:pos] + '# include "btls.h"\n' + content[pos:]
 
             for array in static_arrays:
-                a = array['array_name']
                 arr_code = search_array(content, array['array_name'])
                 arr_obj = StaticArray(arr_code)
                 for value in array['values'].values():
@@ -131,7 +122,6 @@ class Patch:
 class Macro:
     def __init__(self, c):
         self.c = c
-        # self.value = re.search(r"(\s+[\w\s|()\\]+\\?\n)+\n", c)
         tmp = self.c.split(' ', maxsplit=2)
         self.value = tmp[1] if tmp[0] == '#' else tmp[2]
 
@@ -146,34 +136,18 @@ class Function:
     def __init__(self, c):
         self.c = c
 
-    def change_loop_condition(self, old_cond, new_cond):
-        start_pos = self.c.find(old_cond)
-
     def add_to_end(self, code):
         start_pos = self.c.rfind("return")
         self.c = self.c[:start_pos] + code + '\n\t' + self.c[start_pos:]
 
     def add_to_switch(self, switch_cond, new_case):
-        # print(self.c)
         start_pos = re.search(rf"switch\s*\({switch_cond}\)", self.c).start()
         start_pos = self.c.find("case",  start_pos)
         self.c = self.c[:start_pos] + new_case + self.c[start_pos:]
 
     def add_to_if_cond(self, cond, new_cond):
-        #print(cond)
-        #print(new_cond)
-        #print(self.c)
-        # self.c = re.sub(rf"if\s*{cond}", "if " + new_cond, self.c)
-
         p = re.escape(cond)
-        #print(p)
-        a = re.search(rf"if\s*{p}", self.c)
-        #if cond[:10] == "    else {":
-        #    print(p)
-        #    print(a)
-        #    print(self.c)
         self.c = re.sub(rf"if\s*{p}", "if " + new_cond, self.c)
-        #print(self.c)
 
     def insert_before(self, before_line, new_code):
         start_pos = self.c.find(before_line)
@@ -198,29 +172,19 @@ class StaticArray:
         self.list = self.c[start_pos + 1:end_pos - 1].split('\n')
         while self.list[-1] == '':
             self.list.pop()
-        # print("##########")
-        # print(self.list[-1])
-        # print("##########")
-        # print(self.c_array_def)
-        # print("++++++++++")
+
         last_idx = -1
         if self.list[last_idx][0] == '#':
             last_idx = -2
         res = re.search(r'\s*\{*([\w\d\s\|\n])+(([,\s])*([\w\d\|\n\s]*))+\}*', self.list[last_idx])
-        # print(res)
-        # print(self.list[-1][res.start():res.end()])
 
         if ((self.list[last_idx].find(',', res.end())) == -1 and 
                 ((self.c_array_def == "static const ssl_trace_tbl ssl_sigalg_tbl[] = {\n") or 
                 (self.c_array_def == "static const SIGALG_LOOKUP sigalg_lookup_tbl[] = {\n"))):
             self.list[last_idx] = self.list[last_idx] + ','
 
-        # TO DO!!!!
-        #print(self.c_array_def)
-
         self.spaces_before_item = c[start_pos + 1:re.search(r'[\S]',
                                     c[start_pos + 1:]).start() + start_pos + 1]
-        # print(self.spaces_before_item)
 
     def p2c(self):
         items = ''.join([x + '\n' for x in self.list[:-1]])
@@ -236,40 +200,6 @@ class StaticArray:
 
 def patch_ssl_ciph():
     Patch('patch.json')
-    # filename = 'ssl_ciph.c'  # os.path.join('.', 'ssl_ciph.c')
-    # with open(filename, 'r') as f:
-    #     content = f.read()
-
-    # string_for_search = 'ssl_cipher_table_cipher'
-    # arr = search_array(content, string_for_search)
-    # c_array = StaticArray(arr)
-    # c_array.append2c('{SSL_BELTCTR, NID_belt_ctrt}')
-    # print(arr)
-    # obj_ = Array(arr)
-    # print(obj_.items)
-    # print(obj_.indent)
-    # print(obj_.ended_by_comma)
-    # obj_.add_item('{SSL_BELTCTR, NID_belt_ctrt}')
-    # print(obj_.items)
-    # content = content.replace(arr, c_array.p2c())
-
-    # string_before = '};'
-    # string_for_insert = '    {SSL_BELTCTR, NID_belt_ctrt},\n
-    # {SSL_BELTDWP, NID_belt_dwpt},\n'
-    # search_array(content, string_for_search)
-    # content, idx = insert_string(content, string_for_search,
-    #                   string_for_insert, idx, string_before)
-
-    # with open("out.c", "w") as f:
-    #     f.write(content)
-
-    # if content[start_idx:content.find('\n', start_idx)].find('\\'):
-
 
 if __name__ == '__main__':
-    # filename = 'ssl_ciph.c'  # os.path.join('.', 'ssl_ciph.c')
-    # with open(filename, 'r') as f:
-    #     content = f.read()
-
-    # search_macro(content, 'SSL_aCERT')
     patch_ssl_ciph()
