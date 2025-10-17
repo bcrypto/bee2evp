34fb83e2add363f47a4842b71589528cf0c6a6db
Mikhail Mitskevich <mitskevichmn@gmail.com>
2025-10-17 12:57:54 +0300

    Add HMAC test

--- test/test.c
+++ test/test.c
@@ -26,6 +26,7 @@
 extern bool_t beltDWPTest();
 extern bool_t beltCHETest();
 extern bool_t bashPrgTest();
+extern bool_t HMACTest();
 
 int testCyphers()
 {
@@ -44,6 +45,26 @@ int testCyphers()
 
 /*
 *******************************************************************************
+Тестирование функций OpenSSL
+*******************************************************************************
+*/
+
+extern bool_t HMACTest();
+
+int testFunctions()
+{
+	bool_t code;
+	int ret = 0;
+
+	printf("HMAC(belt-hash): %s\n", (code = HMACTest()) ? "OK" : "Err");
+    ret |= !code;
+
+
+	return ret;
+}
+
+/*
+*******************************************************************************
 main
 *******************************************************************************
 */
@@ -53,6 +74,7 @@ int main()
     int ret = 0;
     OPENSSL_add_all_algorithms_conf();
 	ret |= testCyphers();
+    ret |= testFunctions();
     EVP_cleanup();
     ERR_free_strings();
 	return ret;
