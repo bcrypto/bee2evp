f17f6414147ed7a9018ed15a7dc3ae9cbef7c391
Mikhail Mitskevich <mitskevichmn@gmail.com>
2026-04-01 13:12:05 +0300

    Update bee2evp version

--- include/bee2evp/info.h
+++ include/bee2evp/info.h
@@ -23,7 +23,7 @@
 
 #define BEE2EVP_NAME		  "Bee2evp"
 #define BEE2EVP_VERSION_MAJOR "1"
-#define BEE2EVP_VERSION_MINOR "1"
+#define BEE2EVP_VERSION_MINOR "2"
 #define BEE2EVP_VERSION_PATCH "0"
 
 #define BEE2EVP_VERSION                                                        \
@@ -36,7 +36,7 @@
 *******************************************************************************
 \mainpage Криптографический плагин Bee2evp для OpenSSL
 
-\version 1.1.0
+\version 1.2.0
 
 \section toc Содержание
 
